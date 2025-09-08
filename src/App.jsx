9524b4addef32a59bed99d74e2926e09ad64960b
Ashlee Shum <ashu443@aucklanduni.ac.nz>
2025-09-08 19:54:19 +1200

    feat: made navbar component and need to commit before i forget

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
+import LandingPage from './components/LandingPage.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div className='flex'>hello</div>
-    </>
+    
+    <div>
+      <Navbar />
+      <LandingPage />
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <div className ="w-full h-18 bg-transparent border flex items-center justify-center">
+            {/* nav links rhs */}
+            <div className = "">
+
+            </div>
+            {/* logo lhs */}
+            <div className="">
+
+            </div>
+        </div>
+    )
+}
+export default Navbar
